09c2a7aaf1bad2ea5e06ef2fb4656889f015f359
Vijay Bhat <vijay.bhat@capitalone.com>
2015-03-11 18:14:13 -0700

    fixing calls to deprecated iam-tools package to equivalent aws-cli package commands

--- aws/aws-setup-iam.sh
+++ aws/aws-setup-iam.sh
@@ -10,7 +10,7 @@ puts "+----------------------------------------+"
 puts "| CREATING AWS IAM USERS FOR WIRBELSTURM |"
 puts "+----------------------------------------+"
 
-IAM_PATH="/wirbelsturm"
+IAM_PATH="/wirbelsturm/"
 IAM_GROUP="wirbelsturm"
 IN_INSTANCE_USER="wirbelsturm-ininstance"
 IN_INSTANCE_USER_POLICY_FILE=$MY_DIR/in-instance-iam-user.json
@@ -22,36 +22,39 @@ TIMESTAMP=`date +"%Y%m%d-%H%M%S"`
 ### IAM Group
 ###
 
-puts "Creating Wirbelsturm IAM group..."
-iam-groupcreate -g $IAM_GROUP -p $IAM_PATH || exit 1
+puts "Creating Wirbelsturm IAM group ..."
+aws iam create-group --group-name $IAM_GROUP --path $IAM_PATH || exit 1
 
 ###
 ### Deploy IAM user
 ###
 
 puts "Creating deploy IAM user '$DEPLOY_USER'..."
-iam-usercreate -u $DEPLOY_USER -g $IAM_GROUP -p $IAM_PATH || exit 1
-KEYS=`iam-useraddkey -u $DEPLOY_USER`
-DEPLOY_USER_ACCESS_KEY=`echo $KEYS | awk '{ print $1 }'`
-DEPLOY_USER_SECRET_KEY=`echo $KEYS | awk '{ print $2 }'`
+aws iam create-user --user-name $DEPLOY_USER
+aws iam add-user-to-group --group-name $IAM_GROUP --user-name $DEPLOY_USER || exit 1
+KEYS=`aws iam create-access-key --user-name $DEPLOY_USER`
+DEPLOY_USER_ACCESS_KEY=`echo $KEYS | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["AccessKey"]["AccessKeyId"]'`
+DEPLOY_USER_SECRET_KEY=`echo $KEYS | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["AccessKey"]["SecretAccessKey"]'`
+
 
 puts "Applying security policy to IAM user '$DEPLOY_USER'..."
-iam-useruploadpolicy -u $DEPLOY_USER -f $DEPLOY_USER_POLICY_FILE \
-  -p "Deploy_IAM_user_of_Wirbelsturm_v$TIMESTAMP" || exit 1
+aws iam put-user-policy --user-name $DEPLOY_USER --policy-document file://$DEPLOY_USER_POLICY_FILE \
+  --policy-name "Deploy_IAM_user_of_Wirbelsturm_v$TIMESTAMP" || exit 1
 
 ###
 ### In-instance IAM user
 ###
 
 puts "Creating in-instance IAM user '$IN_INSTANCE_USER'..."
-iam-usercreate -u $IN_INSTANCE_USER -g $IAM_GROUP -p $IAM_PATH || exit 1
-KEYS=`iam-useraddkey -u $IN_INSTANCE_USER`
-IN_INSTANCE_USER_ACCESS_KEY=`echo $KEYS | awk '{ print $1 }'`
-IN_INSTANCE_USER_SECRET_KEY=`echo $KEYS | awk '{ print $2 }'`
+aws iam create-user --user-name $IN_INSTANCE_USER
+aws iam add-user-to-group --group-name $IAM_GROUP --user-name $IN_INSTANCE_USER || exit 1
+KEYS=`aws iam create-access-key --user-name $IN_INSTANCE_USER`
+IN_INSTANCE_USER_ACCESS_KEY=`echo $KEYS | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["AccessKey"]["AccessKeyId"]'`
+IN_INSTANCE_USER_SECRET_KEY=`echo $KEYS | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["AccessKey"]["SecretAccessKey"]'`
 
 puts "Applying security policy to IAM user '$IN_INSTANCE_USER'..."
-iam-useruploadpolicy -u $IN_INSTANCE_USER -f $IN_INSTANCE_USER_POLICY_FILE \
-  -p "In-instance_IAM_user_of_Wirbelsturm_v$TIMESTAMP" || exit 1
+aws iam put-user-policy --user-name $IN_INSTANCE_USER --policy-document file://$IN_INSTANCE_USER_POLICY_FILE \
+  --policy-name "In-instance_IAM_user_of_Wirbelsturm_v$TIMESTAMP" || exit 1
 
 ###
 ### Post installation
